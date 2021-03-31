03c3405083d6b60b641378819a8300294ce94684
DESKTOP-QM998KQ\DAt <hoangtiendat.thidaihoc@gmail.com>
2021-03-31 13:29:57 +0700

    done

--- Indexer.c
+++ Indexer.c
@@ -3,59 +3,57 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-typedef struct
-{
+typedef struct {
     char *word;
     int count;
     char *line;
-}Data;
+} WordData;
 
-typedef struct nodetype{
-    Data key;
-    struct nodetype *right,*left;
-}node;
+typedef struct nodetype {
+    WordData key;
+    struct nodetype *right, *left;
+} DataNode;
 
 
 typedef struct nodetype *tree;
-void insertnode(tree *root,Data x)
-{
-    if((*root)==NULL)
-    {
-        *root=(node*)malloc(sizeof(node));
-        (*root)->key=x;
-        (*root)->right=NULL;
-        (*root)->left=NULL;
-    }
-    else if(strcmp(x.word,((*root)->key).word)>0) insertnode(&(*root)->right,x);
-    else if(strcmp(x.word,((*root)->key).word)<0) insertnode(&(*root)->left,x);
-    else{
+
+void insertnode(tree *root, WordData x) {
+    if ((*root) == NULL) {
+        *root = (DataNode *) malloc(sizeof(DataNode));
+        (*root)->key = x;
+        (*root)->right = NULL;
+        (*root)->left = NULL;
+    } else if (strcmp(x.word, ((*root)->key).word) > 0) insertnode(&(*root)->right, x);
+    else if (strcmp(x.word, ((*root)->key).word) < 0) insertnode(&(*root)->left, x);
+    else {
         ((*root)->key).count++;
-        if(strstr(((*root)->key).line,x.line)==NULL) strcat(((*root)->key).line,x.line);
+        if (strstr(((*root)->key).line, x.line) == NULL) strcat(((*root)->key).line, x.line);
     }
 }
 
-tree search(tree root,char *x){
-    if(root==NULL) return NULL;
-    else if(strcmp(x,(root->key).word)>0) search(root->right,x);
-    else if(strcmp(x,(root->key).word)<0) search(root->left,x);
+tree search(tree root, char *x) {
+    if (root == NULL) return NULL;
+    else if (strcmp(x, (root->key).word) > 0) search(root->right, x);
+    else if (strcmp(x, (root->key).word) < 0) search(root->left, x);
     else return root;
 }
 
 
-char * toLowercaseString(char *str){
+char *toLowercaseString(char *str) {
     int len = strlen(str);
-    char * strLowerCase = (char*) malloc(sizeof(char)* len);
-    for(int i=0;i<=strlen(str);i++){
-        if(str[i]>=65&&str[i]<=90){
-            strLowerCase[i]=str[i]+32;
-        }else {
-            strLowerCase[i]=str[i];
-        }        
-        
+    char *strLowerCase = (char *) malloc(sizeof(char) * len);
+    for (int i = 0; i <= strlen(str); i++) {
+        if (str[i] >= 65 && str[i] <= 90) {
+            strLowerCase[i] = str[i] + 32;
+        } else {
+            strLowerCase[i] = str[i];
+        }
+
     }
     return strLowerCase;
 }
-char *ltrim(char *str, const char *seps){
+
+char *ltrim(char *str, const char *seps) {
     size_t totrim;
     if (seps == NULL) {
         seps = "\t\n\v\f\r ";
@@ -65,14 +63,14 @@ char *ltrim(char *str, const char *seps){
         size_t len = strlen(str);
         if (totrim == len) {
             str[0] = '\0';
-        }
-        else {
+        } else {
             memmove(str, str + totrim, len + 1 - totrim);
         }
     }
     return str;
 }
-char *rtrim(char *str, const char *seps){
+
+char *rtrim(char *str, const char *seps) {
     int i;
     if (seps == NULL) {
         seps = "\t\n\v\f\r ";
@@ -84,134 +82,141 @@ char *rtrim(char *str, const char *seps){
     }
     return str;
 }
-char *trim(char *str){
+
+char *trim(char *str) {
     return ltrim(rtrim(str, NULL), NULL);
 }
-int isEmpty(char *str){
-    if (str[0]=='\n' ||str[0]=='\0' || str[0]=='\t')
+
+int isEmpty(char *str) {
+    if (str[0] == '\n' || str[0] == '\0' || str[0] == '\t')
         return 1;
     return 0;
 }
-int hasDigit(char *str){
+
+int hasDigit(char *str) {
     int len = strlen(str);
-    for(int i =0; i < len;i++){
-        if (isdigit(str[i])){
+    for (int i = 0; i < len; i++) {
+        if (isdigit(str[i])) {
             return 1;
         }
-        
+
     }
     return 0;
 }
-int isHasDot(char *str){
+
+int isHasDot(char *str) {
     int len = strlen(str);
-    if (str[len -1]=='.')    {
+    if (str[len - 1] == '.') {
         return 1;
     }
     return 0;
 }
-char* removeDot(char *str){
+
+char *removeDot(char *str) {
     int len = strlen(str);
-    if (isHasDot(str)){
-        str[len -1]='\0';
+    if (isHasDot(str)) {
+        str[len - 1] = '\0';
     }
     return str;
 }
-int isProperNoun(char *str){
-    if (!isEmpty(str) && str[0]>= 'A' && str[0] <= 'Z'){
-        return 1 ;
+
+int isProperNoun(char *str) {
+    if (!isEmpty(str) && str[0] >= 'A' && str[0] <= 'Z') {
+        return 1;
     }
     return 0;
 }
-int compareString(char* word1 ,char* word2){
-    return strcmp(toLowercaseString(trim(word1)),toLowercaseString(trim(word2)));
+
+int compareString(char *word1, char *word2) {
+    return strcmp(toLowercaseString(trim(word1)), toLowercaseString(trim(word2)));
 }
-int isStopWord(char** stopWordList,int stopListSize, char* word){
-    for (int i=0;i< stopListSize; i++){
-        if (compareString(stopWordList[i],word) == 0) return 1;
+
+int isStopWord(char **stopWordList, int stopListSize, char *word) {
+    for (int i = 0; i < stopListSize; i++) {
+        if (compareString(stopWordList[i], word) == 0) return 1;
     }
     return 0;
 }
-FILE * openFile(char *filename, char * type){
+
+FILE *openFile(char *filename, char *type) {
     FILE *fp;
     fp = fopen(filename, type);
-    if (fp ==NULL){
+    if (fp == NULL) {
         printf("Could not open file %s", filename);
     }
     return fp;
 }
-void insertDataString(char * token ,int lineNumer, int columNum, tree *treeWord){
-    tree data = search(*treeWord,token);
-        if (data !=NULL){
-            char *str =(char*) malloc(sizeof(char)*20) ;
-            sprintf(str,",(%d, %d) ",lineNumer, columNum);
-            strcat((data->key).line, str);
-            (data->key).count = (data->key).count+1;
-            free(str);
-        }else{
-            Data words ;
-            words.word = (char*) malloc(sizeof(char)*50);
-            words.line = (char*) malloc(sizeof(char)*10000);
-            strcpy(words.word, token);
-            words.count = 1 ;
-            sprintf(words.line,",(%d, %d) ",lineNumer, columNum);
-            insertnode(treeWord,words);
-         }
+
+void insertDataString(char *token, int lineNumer, tree *treeWord) {
+    tree data = search(*treeWord, token); // tra ve node leaf neu k co, con k tra ve node
+    if (data != NULL) {
+        char *str = (char *) malloc(sizeof(char) * 20);
+        sprintf(str, ",%d, ", lineNumer);
+        strcat((data->key).line, str);
+        (data->key).count = (data->key).count + 1;
+        free(str);
+    } else {
+        WordData words;
+        words.word = (char *) malloc(sizeof(char) * 50);
+        words.line = (char *) malloc(sizeof(char) * 10000);
+        strcpy(words.word, token);
+        words.count = 1;
+        sprintf(words.line, ",%d ", lineNumer);
+        insertnode(treeWord, words);
+    }
 }
 
-void readFileText(char* filename,char** stopWordList,int stopListSize,tree *treeWord)
-{
+void readFileText(char *filename, char **stopWordList, int stopListSize, tree *treeWord) {
     FILE *fp;
     fp = openFile(filename, "r");
     if (fp == NULL) return;
-    char *strLine = (char*) malloc(sizeof(char)*200);
-    char *token = (char*) malloc(sizeof(char)*50);
+    char *strLine = (char *) malloc(sizeof(char) * 200);
+    char *token;
     const char divider[50] = " \n,()-;'`;\"!?:*";
     int lineNumer = 0;
-    int isStartSentence =0;
-    while (fgets(strLine,199,fp)!= NULL){
-        
+    int isStartSentence = 0;
+    while (fgets(strLine, 199, fp) != NULL) {
         lineNumer++;
         token = strtok(trim(strLine), divider);
-        int colNumber = 0;
-        while( token != NULL ) {
-            colNumber++;
-                if (isStartSentence==0){
-                    if (isHasDot(token)){
-                        isStartSentence = 1;
-                    }
-                    token = removeDot(token);
-                }else{
-                    isStartSentence = 0;
-                    if(isStopWord(stopWordList,stopListSize,token)==0 && hasDigit(token) ==0 && isEmpty(token) ==0){
-                        insertDataString(toLowercaseString(token),lineNumer,colNumber, treeWord);
-                    }
-                    continue;
+        while (token != NULL) {
+            if (isStartSentence == 0) {
+                if (isHasDot(token)) {
+                    isStartSentence = 1;
                 }
-                if (isStopWord(stopWordList,stopListSize,token)==0 && hasDigit(token) ==0 && isEmpty(token) ==0 && isProperNoun(token) ==0){
-                    insertDataString(toLowercaseString(token),lineNumer,colNumber,treeWord);
+                token = removeDot(token);
+            } else {
+                isStartSentence = 0;
+                if (isStopWord(stopWordList, stopListSize, token) == 0 && hasDigit(token) == 0 && isEmpty(token) == 0) {
+                    insertDataString(toLowercaseString(token), lineNumer, treeWord);
                 }
+                continue;
+            }
+            if (isStopWord(stopWordList, stopListSize, token) == 0 && hasDigit(token) == 0 && isEmpty(token) == 0 &&
+                isProperNoun(token) == 0) {
+                insertDataString(toLowercaseString(token), lineNumer, treeWord);
+            }
 
             token = strtok(NULL, divider);
-            
+
         }
-       
+
     }
-    
-    fclose(fp); 
+
+    fclose(fp);
 }
 
-char ** getListStopWorld(char *filename,int * numWord){
-    char *str = (char*) malloc(sizeof(char)*49);
-    char** a = (char **)malloc(50 * sizeof(char *));
-    for (int i = 0; i < 50; i++){
-        a[i] = (char *)malloc(50 * sizeof(char));
+char **getListStopWorld(char *filename, int *numWord) {
+    char *str = (char *) malloc(sizeof(char) * 49);
+    char **a = (char **) malloc(50 * sizeof(char *));
+    for (int i = 0; i < 50; i++) {
+        a[i] = (char *) malloc(50 * sizeof(char));
     }
     FILE *fp;
     fp = openFile(filename, "r");
-    if (fp ==NULL)  return NULL;
+    if (fp == NULL) return NULL;
     int i = 0;
-    while(!feof(fp)){
-        fscanf(fp ,"%s\n", str);
+    while (!feof(fp)) {
+        fscanf(fp, "%s\n", str);
         strcpy(a[i], str);
         (i)++;
     }
@@ -220,21 +225,24 @@ char ** getListStopWorld(char *filename,int * numWord){
     fclose(fp);
     return a;
 }
-void displayTree(tree root){
-    if (root!=NULL) {
+
+void displayTree(tree root) {
+    if (root != NULL) {
         displayTree(root->left);
-        printf("\n%-20s%-2i%s",(root->key).word,(root->key).count,(root->key).line);
+        printf("\n%-20s%-2i%s", (root->key).word, (root->key).count, (root->key).line);
         displayTree(root->right);
     }
 }
-void main (void ){
-    char* stopWordFileName = "stopw.txt";
-    char* textFileName = "vanban.txt";
-    int* numStopWord = malloc(sizeof(int));
-    char ** listStopWord = getListStopWorld(stopWordFileName,numStopWord);
-    tree treeWord=NULL ;
-    readFileText(textFileName,listStopWord,*numStopWord,&treeWord);
+
+void main(void) {
+    char *stopWordFileName = "stopw.txt";
+    char *textFileName = "alice30.txt";
+    int *numStopWord = malloc(sizeof(int)); //[]
+    char **listStopWord = getListStopWorld(stopWordFileName, numStopWord); // ["ok", "alo"]
+    tree treeWord = NULL;
+    readFileText(textFileName, listStopWord, *numStopWord, &treeWord);
     displayTree(treeWord);
 
-   
+    // * -> 1 array, 1 string
+    // **
 }
